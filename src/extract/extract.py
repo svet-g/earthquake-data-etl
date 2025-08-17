7cac7cca5cc1c65d1500922873d12fac428238f1
svet-g <svetlana.gljadelkina@gmail.com>
2025-08-17 15:15:23 +0100

    feature: test and implement main extract file and add comments for strech goals

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,2 +1,43 @@
-file_name = 'earthquake-data-last-30-days.geojson'
+import os
+from pathlib import Path
+from requests.exceptions import JSONDecodeError, HTTPError, RequestException
+from src.utils.logging_utils import setup_logger
+from src.utils.extract_utils import extract_initial_month
+
+logger = setup_logger('extract_data', 'extract_data.log')
+
+file_path_data_last_30_days = Path(__file__).parent.parent.parent / 'data' / 'raw' / 'earthquake-data-last-30-days.geojson'
+file_path_tracker_last_30_days = Path(__file__).parent.parent.parent / 'src' / 'extract' / 'poll_tracker.json'
 last_30_days_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+
+def extract(initial_poll_url, file_path_data, file_path_tracker):
+    '''
+    params: 
+        url (string) -> url string for the API call
+        file_path_data (string) -> local path to save the data file to
+        file_path_tracker (string) -> local path to save the tracker file to
+    returns:
+        None
+    '''
+    try:
+        # if tracker file doesn't exists - use the extract intial month util 
+        # to get the initial data in as a geojson
+        if not os.path.isfile(file_path_tracker):
+            extract_initial_month(initial_poll_url, file_path_data, file_path_tracker)
+        # if the file exists - do nothing - 
+        # other functionality will come if get to strech goals
+        else:
+            # strech goal: poll the API either on schedule or using an alert to trigger the extract function
+            # this will use the tracker made in the extract_initial_month function to construct the string for the more general USGS API endpoint
+            pass
+    # handle exceptions relevent to the API call in extract_initial_month
+    # as well as the generic expection last in case of anything unexpected
+    # stretch goal: add retries under relevant exceptions or any other exception handling
+    except JSONDecodeError as e:
+        logger.exception(f'A JSONDecodeError occured: {e}', stack_info=True)
+    except HTTPError as e:
+        logger.exception(f'An HTTPError occured: {e}', stack_info=True)
+    except RequestException as e:
+        logger.exception(f'A RequestException occured: {e}', stack_info=True)
+    except Exception as e:
+        logger.exception(f'An unexpected error occured: {e}', stack_info=True)
---
+++ tests/unit_tests/test_extract.py
@@ -0,0 +1,74 @@
+import os
+from pathlib import Path
+import pytest
+from unittest.mock import patch, MagicMock
+import json
+import logging
+from requests import exceptions
+from src.extract.extract import extract
+
+class TestExtract():
+
+    @patch('src.extract.extract.extract_initial_month')
+    def test_calls_extract_initial_month(self, mocked_extract_intial_month):
+        # arrange
+        file_path_data = Path(__file__).parent.parent.parent / 'data' / 'test' / 'test_extract.geojson'
+        file_path_tracker = Path(__file__).parent.parent.parent / 'test_extract_tracker.json'
+        url = 'fake url'
+        # act
+        extract(url, file_path_data, file_path_tracker)
+        # assert
+        mocked_extract_intial_month.assert_called_once_with(url, file_path_data, file_path_tracker)
+        
+    @patch('src.extract.extract.os.path.isfile')
+    def test_logs_failure_if_JSONDecodeError_raised(self, mocked_file_check, caplog):
+        with caplog.at_level(logging.ERROR):
+            # arrange
+            file_path_data = Path(__file__).parent.parent.parent / 'data' / 'test' / 'test_extract.geojson'
+            file_path_tracker = Path(__file__).parent.parent.parent / 'test_extract_tracker.json'
+            url = 'fake url'
+            mocked_file_check.side_effect = exceptions.JSONDecodeError('msg', 'doc', 1)
+            # act
+            extract(url, file_path_data, file_path_tracker)
+            # assert
+            assert 'A JSONDecodeError occured' in caplog.text
+    
+    @patch('src.extract.extract.os.path.isfile')
+    def test_logs_failure_if_HTTPError_raised(self, mocked_file_check, caplog):
+        with caplog.at_level(logging.ERROR):
+            # arrange
+            file_path_data = Path(__file__).parent.parent.parent / 'data' / 'test' / 'test_extract.geojson'
+            file_path_tracker = Path(__file__).parent.parent.parent / 'test_extract_tracker.json'
+            url = 'fake url'
+            mocked_file_check.side_effect = exceptions.HTTPError
+            # act
+            extract(url, file_path_data, file_path_tracker)
+            # assert
+            assert 'An HTTPError occured' in caplog.text
+
+    @patch('src.extract.extract.os.path.isfile')
+    def test_logs_failure_if_RequestException_raised(self, mocked_file_check, caplog):
+        with caplog.at_level(logging.ERROR):
+            # arrange
+            file_path_data = Path(__file__).parent.parent.parent / 'data' / 'test' / 'test_extract.geojson'
+            file_path_tracker = Path(__file__).parent.parent.parent / 'test_extract_tracker.json'
+            url = 'fake url'
+            mocked_file_check.side_effect = exceptions.RequestException
+            # act
+            extract(url, file_path_data, file_path_tracker)
+            # assert
+            assert 'A RequestException occured:' in caplog.text
+    
+    @patch('src.extract.extract.os.path.isfile')
+    def test_logs_failure_if_RequestException_raised(self, mocked_file_check, caplog):
+        with caplog.at_level(logging.ERROR):
+            # arrange
+            file_path_data = Path(__file__).parent.parent.parent / 'data' / 'test' / 'test_extract.geojson'
+            file_path_tracker = Path(__file__).parent.parent.parent / 'test_extract_tracker.json'
+            url = 'fake url'
+            mocked_file_check.side_effect = Exception
+            # act
+            extract(url, file_path_data, file_path_tracker)
+            # assert
+            assert 'An unexpected error occured:' in caplog.text
+    
