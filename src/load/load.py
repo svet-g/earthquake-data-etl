6698acafb36a361b7e621ec03c5f6b3cb0eb871a
svet-g <svetlana.gljadelkina@gmail.com>
2025-08-19 15:12:45 +0100

    write and test load func

--- src/load/load.py
+++ src/load/load.py
@@ -13,3 +13,24 @@ sig - bigint
 magtype - varchar
 geometry - break up and put back together for streamlit
 '''
+
+def load(df, engine, table_name, schema, mode):
+    '''
+    
+    loads pandas dataframe to a specified table and schema, creates table if doesn't exists, ovewrite entirely if it does exist
+    
+    params:
+        df (pd.DataFrame) - dataframe to load
+        table_name (str) - the name of the table to load to
+        schema (str) - schema name to load to
+        mode (str) - 'fail', 'replace' or 'append' - same as arguments for 'if_exits' in pandas DataFrame.to_sql
+    
+    '''
+
+    with engine.connect() as connection:
+        df.to_sql(table_name, connection, schema=schema, if_exists=mode, index=False)
+        
+        # FROM DOCS - just how to use the result
+        # result = connection.execute(text("select username from users"))
+        # for row in result:
+        #     print("username:", row.username)
---
+++ tests/unit_tests/test_load.py
@@ -0,0 +1,42 @@
+import os
+from sqlalchemy import Engine
+import pandas as pd
+from sqlalchemy import text, create_engine
+from src.utils.load_utils import db_engine
+from src.load.load import load
+
+class TestLoad:
+    # currently pulling out json so the dates are in unix epoch cause to_json() removed it
+    # need to check datetime format later
+    def test_creates_specified_table_in_specified_schema_in_specified_db(self, test_processed_df):
+        # arrange
+        engine = create_engine("sqlite:///:memory:")
+        table_name = 'test'
+        schema = None
+        mode = 'replace'
+        expected_columns = ['id',
+                            'mag',
+                            'place',
+                            'time',
+                            'updated',
+                            'url',
+                            'felt',
+                            'cdi',
+                            'alert',
+                            'tsunami',
+                            'sig',
+                            'magType',
+                            'longitude',
+                            'latitude',
+                            'depth',
+                            'time_to_report',
+                            'depth_group']
+        # act
+        load(test_processed_df, engine, table_name, schema, mode)
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT * FROM test"))
+            actual_columns = [col for col in result.keys()]
+            # print(result.fetchone())
+        # assert
+        assert expected_columns == actual_columns
+        
